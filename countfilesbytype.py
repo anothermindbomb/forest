1b53b5761854f0add260c934dab4f21e38b68985
Steve Flynn <anothermindbomb@gmail.com>
2017-12-19 21:34:47 +0000

    correct the backsalshes and make sure we're case insensitive

--- countfilesbytype.py
+++ countfilesbytype.py
@@ -7,19 +7,19 @@ Steve 10/11/2017
 
 import os
 
-source = r'D:\output\'
-filetypes = {}
+source = r'D:/output/'
+file_types = {}
 
 if __name__ == '__main__':
 
     for root, dirs, files in os.walk(source):
         for file in files:
             filename, suffix = os.path.splitext(file)
-            suffix = suffix.lower()
-            if filetypes.get(suffix) is None:
-                filetypes[suffix] = 1
+            suffix = suffix.lower()  # ensure case insensitivity
+            if file_types.get(suffix) is None:
+                file_types[suffix] = 1
             else:
-                filetypes[suffix] = filetypes.get(suffix) + 1
+                file_types[suffix] = file_types.get(suffix) + 1
 
-for eachentry in sorted(filetypes):
-    print(eachentry, ":", filetypes[eachentry])
+for each_entry in sorted(file_types):
+    print(each_entry, ":", file_types[each_entry])
