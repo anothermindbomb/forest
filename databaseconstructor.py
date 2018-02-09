6eb87036fbc0268347a5e1c8a50e5084839dab50
Steve Flynn <anothermindbomb@gmail.com>
2018-02-09 19:28:22 +0000

    Tidy up the tablename, fix a couple of typos in the comments.

--- databaseconstructor.py
+++ databaseconstructor.py
@@ -8,7 +8,6 @@ import sqlite3
 import os
 
 database_name = 'dolphincommands'
-driver_table_name = 'transactions'
 command_filename = 'executioncommands.txt'
 
 '''
@@ -17,43 +16,41 @@ we always delete any existing database within the main driver loop. Bare bones..
 '''
 
 
-def create_database_table(database_name, driver_table_name):
-    db = sqlite3.connect(database_name)
+def create_database_table(db_name):
+    db = sqlite3.connect(db_name)
     cursor = db.cursor()
     cursor.execute('''
-CREATE TABLE driver_table_name
+CREATE TABLE transactions
 (
-	docid TEXT
-		PRIMARY KEY,
-	update_sql TEXT,
-	del_and_link_cmd TEXT,
-	is_processed BOOLEAN,
-	error_message TEXT
-)
-;
-''')
+  docid TEXT
+    PRIMARY KEY,
+  update_sql TEXT,
+  del_and_link_cmd TEXT,
+  is_processed BOOLEAN,
+  error_message TEXT
+)''')
     db.commit()
     db.close()
 
 
 '''
-This function reads the executioncommands file, 3 lines at a time and inserts them into the transaction table
+This function reads the "executioncommands.txt" file, 3 lines at a time and inserts them into the transaction table
 It assumes the commands are in "docid, sql, link" order and the docid must be unique, as it's our primary key.
 
-Any duplicate documentid, will simply cause this to error out. It's one-shot code so any duplicate docid needs
-to be looked at anyawy, so we don't bother with any error handling.  
+Any duplicate docid, will simply cause this to error out. It's one-shot code so any duplicate docid needs
+to be looked at anyway, so we don't bother with any error handling.  
 '''
 
 
-def insert_command_file(command_filename):
+def insert_command_file(cmd_filename):
     db = sqlite3.connect(database_name)
     cursor = db.cursor()
-    with open(command_filename) as f:
+    with open(cmd_filename) as f:
         for line in f:
             docid = line
             sqlcmd = f.readline()
             linkcmd = f.readline()
-            cursor.execute('INSERT INTO "driver_table_name"(docid, update_sql, del_and_link_cmd) VALUES (?,?,?)',
+            cursor.execute('INSERT INTO transactions (docid, update_sql, del_and_link_cmd) VALUES (?,?,?)',
                            (docid, sqlcmd, linkcmd))
             db.commit()
     db.close()
@@ -61,7 +58,7 @@ def insert_command_file(command_filename):
 
 if __name__ == '__main__':
     os.remove(database_name)  # remove it if an old one exists
-    create_database_table(database_name, driver_table_name)  # create the database and the table within it.
+    create_database_table(database_name)  # create the database.
     insert_command_file(command_filename)  # read the list of commands to be executed ad stick them in the sqlite3 table
 
     # we're done!
